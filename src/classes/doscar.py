b281b7b0c8268069544ec2a40ef05c2791415465
Zhao Tang <tangzhao20@gmail.com>
2024-11-04 17:23:07 -0600

    Allowed partial occupation in eig0.py for QE

--- src/classes/doscar.py
+++ src/classes/doscar.py
@@ -3,6 +3,7 @@ import os
 import re
 import numpy as np
 import xml.etree.ElementTree as ET
+from load_data import load_constant
 
 class Doscar:
     # ef : float
@@ -12,12 +13,12 @@ class Doscar:
     # dos[Ns][Nedos]
 
     def __init__(self):
-        self.ef=0.0
-        self.Ns=0
-        self.Nedos=0
-        self.energy=[]
-        self.dos=[[]]
-        self.Lpdos=False
+        self.ef = 0.0
+        self.Ns = 0
+        self.Nedos = 0
+        self.energy = []
+        self.dos = [[]]
+        self.Lpdos = False
 
     def __str__(self):
         str_out = "DOSCAR:\n"
@@ -28,66 +29,66 @@ class Doscar:
 #######################################################################
 
     def read_vasp(self, filename="DOSCAR"):
-        f0=open(filename,"r")
-        line=f0.readlines()
+        f0 = open(filename, "r")
+        line = f0.readlines()
         f0.close()
-        if len(line)<7 :
+        if len(line) < 7:
             print("Error: incomplete DOSCAR")
             sys.exit()
-        self.Nedos=int(line[5].split()[2])
-        self.ef=float(line[5].split()[3])
-        self.Ns=(len(line[6].split())-1)//2
-        self.energy=[0.0]*self.Nedos
-        if self.Ns==1 :
-            self.dos=[[0.0]*self.Nedos]
-        else :
-            self.dos=[[0.0]*self.Nedos,[0.0]*self.Nedos]
+        self.Nedos = int(line[5].split()[2])
+        self.ef = float(line[5].split()[3])
+        self.Ns = (len(line[6].split()) - 1)//2
+        self.energy = [0.0]*self.Nedos
+        if self.Ns == 1:
+            self.dos = [[0.0]*self.Nedos]
+        else:
+            self.dos = [[0.0]*self.Nedos, [0.0]*self.Nedos]
 
-        for il in range(self.Nedos) :
-            word=line[il+6].split()
-            self.energy[il]=float(word[0])
-            self.dos[0][il]=float(word[1])
-            if self.Ns==2 :
-                self.dos[1][il]=float(word[2])
+        for il in range(self.Nedos):
+            word = line[il + 6].split()
+            self.energy[il] = float(word[0])
+            self.dos[0][il] = float(word[1])
+            if self.Ns == 2:
+                self.dos[1][il] = float(word[2])
 
     def read_xml(self, filename=""):
         # read dos from .xml file
-        Ha=load_constant("rydberg")*2.0
+        Ha = load_constant("rydberg")*2.0
 
-        if filename=="" :
+        if filename == "":
             # find a .xml file
             files = os.listdir()
             for f in files:
                 if f.endswith('.xml'):
-                    filename=f
+                    filename = f
                     break
-        tree=ET.parse(filename)
+        tree = ET.parse(filename)
         #TODO: test the metal case
-        if tree.getroot().find("output").find("band_structure").find("highestOccupiedLevel") is None :
-            self.ef=float(tree.getroot().find("output").find("band_structure").find("fermi_energy").text)*Ha
+        if tree.getroot().find("output").find("band_structure").find("highestOccupiedLevel") is None:
+            self.ef = float(tree.getroot().find("output").find("band_structure").find("fermi_energy").text)*Ha
         else :
-            self.ef=float(tree.getroot().find("output").find("band_structure").find("highestOccupiedLevel").text)*Ha
+            self.ef = float(tree.getroot().find("output").find("band_structure").find("highestOccupiedLevel").text)*Ha
 
     def read_qe(self, filename="pwscf.dos"):
-        f0=open(filename,"r")
-        line=f0.readlines()
+        f0 = open(filename, "r")
+        line = f0.readlines()
         f0.close()
 
         if len(line)<2 :
-            print("Error: incomplete "+filename)
+            print("Error: incomplete " + filename)
             sys.exit()
-        self.ef=float(line[0].split()[-2])
-        self.Ns=(len(line[1].split())-1)//2
-        if self.Ns==2 :
+        self.ef = float(line[0].split()[-2])
+        self.Ns = (len(line[1].split()) - 1)//2
+        if self.Ns == 2:
             self.dos.append([])
-        for l in line :
-            word=l.split()
-            if len(word)==0 or word[0][0]=="#" or word[0][0]=="!" :
+        for l in line:
+            word = l.split()
+            if len(word) == 0 or word[0][0] == "#" or word[0][0] == "!":
                 continue
-            self.Nedos+=1
+            self.Nedos += 1
             self.energy.append(float(word[0]))
             self.dos[0].append(float(word[1]))
-            if self.Ns==2 :
+            if self.Ns == 2:
                 self.dos[1].append(float(word[2]))
 
     def readpdos_qe(self):
@@ -101,149 +102,149 @@ class Doscar:
             print("QE pdos has not been supported for spin polarized system yet.")
             sys.exit()
 
-        files=os.listdir()
+        files = os.listdir()
         pdoslist = [s for s in files if ".pdos_atm#" in s]
-        filelist=[]
+        filelist = []
         for f in files:
             fileinfo = re.search(r"(.+?)\.pdos_atm#(\d+)\((.+?)\)_wfc#(\d+)\((.+?)\)", f)
             if fileinfo :
-                filename=fileinfo.group(0)
-                system=fileinfo.group(1)
-                iatom=fileinfo.group(2)
-                atomtype=fileinfo.group(3)
-                iorbit=fileinfo.group(4)
-                orbittype=fileinfo.group(5)
+                filename = fileinfo.group(0)
+                system = fileinfo.group(1)
+                iatom = fileinfo.group(2)
+                atomtype = fileinfo.group(3)
+                iorbit = fileinfo.group(4)
+                orbittype = fileinfo.group(5)
                 filelist.append([filename, int(iatom), atomtype, int(iorbit)])
-        if len(filelist)==0 :
+        if len(filelist) == 0 :
             print("Error: No valid pdos file found.")
             sys.exit()
         filelist = sorted(filelist, key=lambda x: (x[1], x[3]))
-        self.Na=max(filelist, key=lambda x: x[1])[1]
-        self.Nlm=max(filelist, key=lambda x: x[3])[3]**2
+        self.Na = max(filelist, key=lambda x: x[1])[1]
+        self.Nlm = max(filelist, key=lambda x: x[3])[3]**2
 
         # Read energies and Nepdos from the first file
-        f=filelist[0]
-        f0=open(f[0],"r")
-        line=f0.readlines()
+        f = filelist[0]
+        f0 = open(f[0], "r")
+        line = f0.readlines()
         f0.close()
-        self.energy_pdos=[]
-        for l in line :
-            word=l.split()
-            if len(word)==0 or word[0][0]=="#" or word[0][0]=="!" :
+        self.energy_pdos = []
+        for l in line:
+            word = l.split()
+            if len(word) == 0 or word[0][0] == "#" or word[0][0] == "!":
                 continue
             self.energy_pdos.append(float(word[0]))
-        self.Nepdos=len(self.energy_pdos)
+        self.Nepdos = len(self.energy_pdos)
         
-        self.pdos=[]
-        self.pdos=np.arange(self.Ns*self.Nepdos*self.Na*self.Nlm)
-        self.pdos=np.reshape(self.pdos,(self.Ns,self.Nepdos,self.Na,self.Nlm)).tolist()
+        self.pdos = []
+        self.pdos = np.arange(self.Ns*self.Nepdos*self.Na*self.Nlm)
+        self.pdos = np.reshape(self.pdos, (self.Ns, self.Nepdos, self.Na, self.Nlm)).tolist()
 
         for f in filelist:
             # f=[filename, iatom, atomtype, iorbit]
-            f0=open(f[0],"r")
-            line=f0.readlines()
+            f0 = open(f[0], "r")
+            line = f0.readlines()
             f0.close()
-            ie=0
-            for l in line :
-                word=l.split()
-                if len(word)==0 or word[0][0]=="#" or word[0][0]=="!" :
+            ie = 0
+            for l in line:
+                word = l.split()
+                if len(word) == 0 or word[0][0] == "#" or word[0][0] == "!":
                     continue
-                if f[3]==1 :
-                    self.pdos[0][ie][f[1]-1][0]=float(word[2])
-                elif f[3]==2 :
-                    self.pdos[0][ie][f[1]-1][1:4]=[float(word[2]),float(word[3]),float(word[4])]
-                ie+=1
+                if f[3] == 1:
+                    self.pdos[0][ie][f[1] - 1][0] = float(word[2])
+                elif f[3] == 2:
+                    self.pdos[0][ie][f[1] - 1][1:4]=[float(word[2]), float(word[3]), float(word[4])]
+                ie += 1
 
         # build atom list atomtype[Ntype] and Naint[Ntype]
-        self.atomtype=[]
-        self.Naint=[]
-        for f in filelist :
-            if f[3]==1 :
-                if len(self.atomtype)==0 or self.atomtype[-1]!=f[2] :
+        self.atomtype = []
+        self.Naint = []
+        for f in filelist:
+            if f[3] == 1:
+                if len(self.atomtype) == 0 or self.atomtype[-1] != f[2]:
                     self.atomtype.append(f[2])
                     self.Naint.append(1)
                 else :
-                    self.Naint[-1]+=1
+                    self.Naint[-1] += 1
 
         # Only s and p orbitals are supported now
-        self.orb_name=["s","pz","px","py"]
-        self.Lpdos=True
+        self.orb_name = ["s", "pz", "px", "py"]
+        self.Lpdos = True
                 
 #######################################################################
 
     def energyshift(self, ezero):
         for ie in range(self.Nedos):
-            self.energy[ie]=self.energy[ie]-ezero
-        if self.Lpdos :
+            self.energy[ie] = self.energy[ie] - ezero
+        if self.Lpdos:
             for ie in range(self.Nepdos):
-                self.energy_pdos[ie]=self.energy_pdos[ie]-ezero
+                self.energy_pdos[ie] = self.energy_pdos[ie] - ezero
             
     def plot_pdos(self, atom_flag, orb_flag):
-        pdos_out=[]
+        pdos_out = []
         for ie in range(self.Nepdos):
-            pdos_out0=0.0
+            pdos_out0 = 0.0
             for ia in range(self.Na):
-                for im in range(self.Nlm) :
-                    if atom_flag[ia] and orb_flag[im]==1 :
-                        pdos_out0+=self.pdos[0][ie][ia][im]
+                for im in range(self.Nlm):
+                    if atom_flag[ia] and orb_flag[im] == 1:
+                        pdos_out0 += self.pdos[0][ie][ia][im]
             pdos_out.append(pdos_out0)
         return pdos_out
 
     def read_orb_list(self, orb_list):
-        orb_list0=orb_list.split("+")
-        orb_list1=[]
-        orb_flag=[0]*self.Nlm
-        for j in orb_list0 :
+        orb_list0 = orb_list.split("+")
+        orb_list1 = []
+        orb_flag = [0]*self.Nlm
+        for j in orb_list0:
             if j in self.orb_name:
                 orb_list1.append(j)
-            elif j=="p" :
-                orb_list1+=["px","py","pz"]
-            elif j=="d" :
-                orb_list1+=["dxy","dyz","dz2","dxz","x2-y2"]
-            elif j=="f" :
-                orb_list1+=["fy3x2","fxyz","fyz2","fz3","fxz2","fzx2","fx3"]
-            elif j=="dx2-y2" :
+            elif j == "p":
+                orb_list1 += ["px", "py", "pz"]
+            elif j == "d":
+                orb_list1 += ["dxy", "dyz", "dz2", "dxz", "x2-y2"]
+            elif j == "f":
+                orb_list1 += ["fy3x2", "fxyz", "fyz2", "fz3", "fxz2", "fzx2", "fx3"]
+            elif j == "dx2-y2":
                 orb_list1.append("x2-y2")
-            elif j=="all" :
-                orb_list1+=self.orb_name
+            elif j == "all":
+                orb_list1 += self.orb_name
             else :
-                print("projector "+j+" does not exist")
+                print("projector " + j + " does not exist")
 
         for j in orb_list1:
             if j in self.orb_name:
-                orb_flag[self.orb_name.index(j)]=1
-            else :
-                print("projector "+j+" does not exist")
+                orb_flag[self.orb_name.index(j)] = 1
+            else:
+                print("projector " + j + " does not exist")
 
         return orb_flag
 
     def read_atom_list(self, atom_list):
-        atom_flag=[]
-        if atom_list=="all" :
+        atom_flag = []
+        if atom_list == "all":
             for a in range(self.Na):
                 atom_flag.append(1)
         else :
             for a in range(self.Na):
                 atom_flag.append(0)
-            atom_list0=atom_list.split(",")
+            atom_list0 = atom_list.split(",")
             for i in range(len(atom_list0)):
-                Fatom_valid=False
-                Iatom0=0
-                Iatom1=0
+                Fatom_valid = False
+                Iatom0 = 0
+                Iatom1 = 0
                 for j in range(len(self.atomtype)):
-                    Iatom1=Iatom1+self.Naint[j]
+                    Iatom1 = Iatom1 + self.Naint[j]
                     if atom_list0[i] == self.atomtype[j]:
-                        for a in range(Iatom0,Iatom1):
-                            atom_flag[a]=1
-                        Fatom_valid=True
-                    Iatom0=Iatom1
+                        for a in range(Iatom0, Iatom1):
+                            atom_flag[a] = 1
+                        Fatom_valid = True
+                    Iatom0 = Iatom1
                 if not Fatom_valid:
-                    atom_list1=atom_list0[i].split("-")
-                    if len(atom_list1)==1 :
-                        atom_flag[int(atom_list1[0])-1]=1
-                        Fatom_valid=True
+                    atom_list1 = atom_list0[i].split("-")
+                    if len(atom_list1) == 1:
+                        atom_flag[int(atom_list1[0])-1] = 1
+                        Fatom_valid = True
                     else :
                         for j in range(int(atom_list1[0])-1, int(atom_list1[1])):
-                            atom_flag[j]=1
-                        Fatom_valid=True
+                            atom_flag[j] = 1
+                        Fatom_valid = True
         return atom_flag
--- src/eig0.py
+++ src/eig0.py
@@ -9,109 +9,114 @@
 
 import os
 import sys
-from classes import Eigenval
+from classes import Eigenval, Doscar
 from load_data import load_package_name, load_palette
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-fsecond=False
+fsecond = False
 
-if len(sys.argv)<=1 :
+if len(sys.argv) <= 1:
     print("python bands.py package (Emin) (Emax)")
     sys.exit()
-package=sys.argv[1]
+package = sys.argv[1]
 
-package_name=load_package_name()
+package_name = load_package_name()
 
-eigenval1=Eigenval()
-if package in package_name["vasp"] : 
+eigenval1 = Eigenval()
+if package in package_name["vasp"]:
     # Input: EIGENVAL
     # TODO: test VASP
     eigenval1.read_vasp()
     
-elif package in package_name["qe"] :
+elif package in package_name["qe"]:
     # Input: *.xml
     eigenval1.read_qexml()
 
-elif package in package_name["parsec"] :
+elif package in package_name["parsec"]:
     # Input: bands.dat
     eigenval1.read_parsec()
 
 else:
-    print("Package \""+package+"\" is not supported yet.")
+    print("Package \"" + package + "\" is not supported yet.")
     print("python bands.py package (Emin) (Emax)")
     sys.exit()
 
-if eigenval1.is_semic==True :
+if eigenval1.is_semic == True:
     eigenval1.eigshift(eigenval1.vbm)
 else :
-    print("Error: only semiconductor with bandgap is supported")
-    sys.exit()
+    if package in package_name["qe"]:
+        doscar1 = Doscar()
+        doscar1.read_xml()
+        eigenval1.eigshift(doscar1.ef)
+    else:
+        print("Error: only semiconductor with bandgap is supported")
+        sys.exit()
 
-ik_gamma=-1
-for ik in range(eigenval1.Nk) :
-    summ=0
-    for ix in range(3) :
-        summ+=abs(eigenval1.kp[ik][ix])
-    if summ<1e-6 :
-        ik_gamma=ik
+ik_gamma = -1
+for ik in range(eigenval1.Nk):
+    summ = 0
+    for ix in range(3):
+        summ += abs(eigenval1.kp[ik][ix])
+    if summ < 1e-6:
+        ik_gamma = ik
         break
-if ik_gamma==-1 :
+if ik_gamma == -1:
     print("Error: The Gamma point is not found")
     sys.exit()
 
-if len(sys.argv)>=4 :
-    ymax=float(sys.argv[3])
-    ymin=float(sys.argv[2])
-elif len(sys.argv)==3 :
-    ymax=float(sys.argv[2])
-    ymin=-float(sys.argv[2])
+if len(sys.argv) >= 4:
+    ymax = float(sys.argv[3])
+    ymin = float(sys.argv[2])
+elif len(sys.argv) == 3:
+    ymax = float(sys.argv[2])
+    ymin = -float(sys.argv[2])
 else :
-    ymax=5.0
-    ymin=-5.0
+    ymax = 5.0
+    ymin = -5.0
 
-palette=load_palette()
-mpl.rcParams["font.sans-serif"].insert(0,"Noto Sans")
+palette = load_palette()
+mpl.rcParams["font.sans-serif"].insert(0, "Noto Sans")
 mpl.rcParams.update({'font.size': 14})
 
 # band structure plot
 
-linecolor=["darkblue","orange"]
+linecolor = ["darkblue", "orange"]
 
-fig=plt.figure(figsize=(1.8,3.75))
-gs0=fig.add_gridspec(1,eigenval1.Ns,wspace=0.0,hspace=0.00,left=0.45,right=0.97,top=0.97, bottom=0.07)
+fig = plt.figure(figsize=(1.8, 3.75))
+gs0 = fig.add_gridspec(1, eigenval1.Ns, wspace=0.0, hspace=0.00, left=0.45, right=0.97, top=0.97, bottom=0.07)
 
-ax=[]
+ax = []
 for ispin in range(eigenval1.Ns):
     ax.append(fig.add_subplot(gs0[ispin]))
 
-    ax[ispin].grid(axis="x",linewidth=1, color=palette["gray"],zorder=0)
-    ax[ispin].axhline(linewidth=1,color=palette["gray"],zorder=0)
+    ax[ispin].grid(axis="x", linewidth=1, color=palette["gray"], zorder=0)
+    ax[ispin].axhline(linewidth=1, color=palette["gray"], zorder=0)
 
-    for ib in range(eigenval1.Nb) :
-        if eigenval1.occ[ik_gamma][ib][ispin] >0.5 :
-            linestyle="solid"
-        else :
-            linestyle="dashed"
-        ax[ispin].axhline(y=eigenval1.eig[ik_gamma][ib][ispin],linestyle=linestyle,linewidth=1,color=palette[linecolor[ispin]],zorder=2)
+    for ib in range(eigenval1.Nb):
+        if eigenval1.occ[ik_gamma][ib][ispin] > 0.5:
+            linestyle = "solid"
+        else:
+            linestyle = "dashed"
+        ax[ispin].axhline(y=eigenval1.eig[ik_gamma][ib][ispin], linestyle=linestyle, linewidth=1, color=palette[linecolor[ispin]], zorder=2)
 
-outputname="eig0.png"
+outputname = "eig0.png"
 
-ax[0].set_ylabel("Energy (eV)",labelpad=-2,color=palette["black"])
+ax[0].set_ylabel("Energy (eV)", labelpad=-2, color=palette["black"])
 for ispin in range(eigenval1.Ns):
-    ax[ispin].set_ylim(ymin,ymax)
-    ax[ispin].set_xlim(0,1)
+    ax[ispin].set_ylim(ymin, ymax)
+    ax[ispin].set_xlim(0, 1)
     ax[ispin].set_xticks([])
     ax[ispin].tick_params(axis="x", direction="in", length=0)
     ax[ispin].tick_params(axis="y", left=False, right=False, direction="in", color=palette["gray"], labelcolor=palette["black"], width=1, zorder=0)
-    if ispin!=0 :
+    if ispin != 0:
         ax[ispin].yaxis.set_ticklabels([])
-    for edge in ["bottom", "top", "left", "right"] :
+    for edge in ["bottom", "top", "left", "right"]:
         ax[ispin].spines[edge].set_color(palette["black"])
         ax[ispin].spines[edge].set_linewidth(1)
         ax[ispin].spines[edge].set_zorder(4)
 ax[0].tick_params(axis="y", left=True)
 ax[-1].tick_params(axis="y", right=True)
 
-fig.savefig(outputname,dpi=1200)
+fig.savefig(outputname, dpi=1200)
 
