a19b7b16bd700b327dfbd7b3cb905f174f158b7f
Zhao Tang <tangzhao20@gmail.com>
2025-08-18 22:48:27 -0500

    Allow custom filename in volume.py

--- src/volume.py
+++ src/volume.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 
-# volume.py
+# volume.py (filename)
 # print the volume of a structure
 
-from classes import Poscar
 import sys
+import os
+from classes import Poscar
+
+filename1 = "POSCAR"
+for iw in range(1, len(sys.argv)):
+    if os.path.isfile(sys.argv[iw]):
+        filename1 = sys.argv[iw]
+        # del sys.argv[iw]
+        break
 
 poscar1 = Poscar()
-poscar1.read_vasp()
+poscar1.read_vasp(filename=filename1)
 
 vol = poscar1.volume()
 
-print(f"volume: {vol:.6f} A^3")
+print(f"volume: {vol:.12f} A^3")
