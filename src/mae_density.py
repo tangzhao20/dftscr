749edb00d88fb7284c78fed2cc14fb880e3adfb0
Zhao Tang <tangzhao20@gmail.com>
2025-11-10 00:57:03 -0600

    wip: Implement energy-resolved MAE density calculation

---
+++ src/mae_density.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+
+# mae_bands.py med easy (E1) (E2)
+# Making the energy-resolved MAE density
+
+# Only for VASP outputs
+
+# if E2 exists, the energy range is (E1,E2)
+# if only E1 exists, the energy range is (-E1,E1)
+# if neither exists, the energy range is (-5 eV, 5 eV)
+
+import sys
+import numpy as np
+from classes import Poscar, Procar
+from load_data import load_constant, load_palette
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+x_str = ["x", "y", "z"]
+
+i_med = x_str.index(sys.argv[1])
+i_easy = x_str.index(sys.argv[2])
+
+y_range = [-5.0, 5.0]
+if len(sys.argv) >= 5:
+    y_range[0] = min(float(sys.argv[3]), float(sys.argv[4]))
+    y_range[1] = max(float(sys.argv[3]), float(sys.argv[4]))
+elif len(sys.argv) == 4:
+    y_range[0] = -float(sys.argv[3])
+    y_range[1] = float(sys.argv[3])
+
+poscar0 = Poscar()
+poscar0.read_vasp(filename="CONTCAR")
+
+procar0 = Procar()
+procar0.read_vasp()
+
+atom_list = poscar0.atom_list()
+
+# SOC factors taken from [New J. Phys. 21, 73054 (2019)]
+soc_factor_dict = {"Fe": 0.05965, "Co": 0.07412}
+
+atom_mask = np.isin(atom_list, list(soc_factor_dict))
+soc_factors = np.array([soc_factor_dict[a] for a in np.array(atom_list)[atom_mask]])
+
+Ld = np.zeros((3, 5, 5))  # i: x, y, z
+# Order of orbs: dxy, dyz, dz2, dxz, x2-y2
+
+Ld[0, 0, 3] = 1.0
+Ld[0, 1, 2] = 3.0**0.5
+Ld[0, 1, 4] = 1.0
+Ld[0, 2, 1] = -3.0**0.5
+Ld[0, 3, 0] = -1.0
+Ld[0, 4, 1] = -1.0
+
+Ld[1, 0, 1] = 1.0
+Ld[1, 1, 0] = -1.0
+Ld[1, 2, 3] = -3.0**0.5
+Ld[1, 3, 2] = 3.0**0.5
+Ld[1, 3, 4] = -1.0
+Ld[1, 4, 3] = 1.0
+
+Ld[2, 0, 4] = -2.0
+Ld[2, 1, 3] = -1.0
+Ld[2, 3, 1] = 1.0
+Ld[2, 4, 0] = 2.0
+
+eta = 0.0001
+
+# prepare DOS data
+sigma = 0.05  # eV
+gaussian_coeff = (1 / (sigma * (2*np.pi)**0.5))
+Nedos = 1000
+energy = np.linspace(y_range[0], y_range[1], Nedos)
+mae_density = np.zeros((2, Nedos))  # Energy, spin
+
+for ik in range(procar0.Nk):
+    weight = procar0.weight[ik]
+    for ispin1 in range(2):
+        for ispin2 in range(2):
+            if ispin1 == ispin2:
+                sign = 1
+            else:
+                sign = -1
+
+            # vectorization over bands
+            e_diff = procar0.eig[ispin1, ik, :][:, np.newaxis] - procar0.eig[ispin2, ik, :][np.newaxis, :]
+            e = e_diff / (e_diff**2 + eta**2)
+            f = procar0.occ[ispin1, ik, :][:, np.newaxis] * (1 - procar0.occ[ispin2, ik, :][np.newaxis, :])
+            # f = procar0.occ[ispin1, ik, :][:, np.newaxis] - procar0.occ[ispin2, ik, :][np.newaxis, :]
+            e = e[np.newaxis, :, :]
+            f = f[np.newaxis, :, :]
+
+            c1 = procar0.complex[ispin1, ik, :, atom_mask, 4:9].conj()  # numpy move the masked axis to front
+            c2 = procar0.complex[ispin2, ik, :, atom_mask, 4:9]
+
+            # a: atoms; i,j: bands; x: directions; m,n: orbitals
+            L = np.einsum("a, aim, xmn, ajn -> xij", soc_factors, c1, Ld, c2)
+            e_i += sign * weight * np.sum(f * e * np.abs(L)**2 * 0.25, axis=(1, 2))
+                       
+            # TODO: calculate MAE density here
+
+mae_density_max = np.max(mae_density)
+mae_density_min = np.min(mae_density)
+mae_density_color = ["darkblue", "orange"]
+
+palette = load_palette()
+mpl.rcParams["font.sans-serif"].insert(0, "Noto Sans")
+mpl.rcParams.update({'font.size': 14})
+
+fig = plt.figure(figsize=(1, 3.75))
+gs0 = fig.add_gridspec(1, 1, wspace=0.0, hspace=0.00, left=0.03, right=0.97, top=0.97, bottom=0.07)
+
+ax=fig.add_subplot(gs0[0])
+
+ax.axhline(linewidth=1, color=palette["gray"], zorder=0)
+for ispin in range(2):
+    ax.plot(mae_density[ispin], energy, color=palette[mae_density_color[ispin]], linewidth=1, zorder=3)
+
+x_range_sum = [0.0, 0.0]
+x_range_sum[0] = mae_density_min - (mae_density_max - mae_density_min) * 0.05
+x_range_sum[1] = mae_density_max + (mae_density_max - mae_density_min) * 0.05
+ax.set_xlim(x_range_sum)
+
+ax.set_ylim(y_range)
+ax.tick_params(axis="x", bottom=False, top=False, direction="in", length=0)
+ax.tick_params(axis="y", left=True, right=True, direction="in", color=palette["gray"], labelcolor=palette["black"], width=1, zorder=0, pad=4)
+ax.set_yticklabels([])
+
+# put "DOS" as ticklabel to align with the K point labels in bs
+ax[0].set_xticks([0.5*(mae_density_max+mae_density_min)], ["MAE Density"], color=palette["black"])
+
+for edge in ["bottom", "top", "left", "right"]:
+    ax.spines[edge].set_color(palette["black"])
+    ax.spines[edge].set_linewidth(1)
+    ax.spines[edge].set_zorder(4)
+
+fig.savefig("mae_density.png", dpi=1200)
+
