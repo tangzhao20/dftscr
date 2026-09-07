ef53f625a52a83cd910f219e02a5f864ece68b0f
Zhao Tang <tangzhao20@gmail.com>
2026-09-07 01:16:18 -0500

    feat: add column-based output for out.py

--- src/out.py
+++ src/out.py
@@ -8,9 +8,10 @@ from classes import Poscar, Outcar
 from load_data import load_constant, load_package_name
 
 if len(sys.argv) <= 1:
-    print("python out.py package")
+    print("python out.py package [prop1 prop2 ...]")
     sys.exit()
 package = sys.argv[1]
+custom_output = sys.argv[2:]
 
 package_name = load_package_name()
 
@@ -31,7 +32,7 @@ elif package in package_name["qe"]:
     poscar0.read_xml()
 else:
     print("Package \""+package+"\" is not supported yet.")
-    print("python out.py package")
+    print("Usage: python out.py package [prop1 prop2 ...]")
     sys.exit()
 
 lengths, angles = poscar0.get_lattice_parameter()
@@ -41,16 +42,47 @@ toten_per_atom = outcar0.toten / poscar0.Natom
 
 Js = outcar0.mag * mu_B / (volume * angstrom**3) * mu_0
 
-print(f"lattice parameters: {lengths[0]:.3f} {lengths[1]:.3f} {lengths[2]:.3f} A")
-print(f"lattice angles: {angles[0]:.1f} {angles[1]:.1f} {angles[2]:.1f} deg")
-print(f"volume: {volume:.3f} A^3")
+if not custom_output:
+    print(f"lattice parameters: {lengths[0]:.3f} {lengths[1]:.3f} {lengths[2]:.3f} A")
+    print(f"lattice angles: {angles[0]:.1f} {angles[1]:.1f} {angles[2]:.1f} deg")
+    print(f"volume: {volume:.3f} A^3")
 
-print()
-print(f"toten: {outcar0.toten:.6f} eV")
-print(f"toten per atom: {toten_per_atom:.6f} eV/atom")
+    print()
+    print(f"toten: {outcar0.toten:.6f} eV")
+    print(f"toten per atom: {toten_per_atom:.6f} eV/atom")
 
-print()
-print(f"mag: {outcar0.mag:.6f} mu_B")
-print(f"J_s: {Js:.3f} T")
+    print()
+    print(f"mag: {outcar0.mag:.6f} mu_B")
+    print(f"J_s: {Js:.3f} T")
 
-# print(f"{lengths[0]:.6f} {lengths[2]:.6f} {volume:.6f} {outcar0.toten:.6f} {outcar0.mag:.6f} {Js:.6f}")
+else:
+    properties = {
+        "a": lengths[0],
+        "b": lengths[1],
+        "c": lengths[2],
+        "alpha": angles[0],
+        "beta": angles[1],
+        "gamma": angles[2],
+        "volume": volume,
+        "toten": outcar0.toten,
+        "toten_per_atom": toten_per_atom,
+        "mag": outcar0.mag,
+        "js": Js,
+    }
+
+    aliases = {
+        "vol": "volume",
+        "e_per_atom": "toten_per_atom",
+        "j_s": "js",
+    }
+
+    output_vals = []
+    for item in custom_output:
+        key = aliases.get(item.lower(), item.lower())
+        if key in properties:
+            val = properties[key]
+            output_vals.append(f"{val:.6f}")
+        else:
+            output_vals.append("Unknown")
+
+    print(" ".join(output_vals))
